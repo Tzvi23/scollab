a05e92fb8f53651160d30406adad8c736a365f63
MIke A <michaelafu@gmail.com>
2019-04-11 19:50:33 +0300

    Added some DB functionality

---
+++ StudentCollab/Dal/FilesDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using StudentCollab.Models;
+
+namespace StudentCollab.Dal
+{
+    public class FilesDal : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Files>().ToTable("filesTable");
+        }
+
+        public DbSet<Files> Institutions { get; set; }
+    }
+}
---
+++ StudentCollab/Models/Files.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using StudentCollab.Models;
+
+namespace StudentCollab.Models
+{
+    public class Files
+    {
+        public int FileID { get; set; }
+    }
+}
